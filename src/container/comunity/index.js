9bcf16dfaef7fbc1eb8d0b984625aeda1e0f777d
Pozitivchik <maxymyurkovets@gmail.com>
2024-11-06 01:19:44 +0200

    complete task

--- src/container/comunity/index.js
+++ src/container/comunity/index.js
@@ -6,77 +6,83 @@ import {
 const page = document.querySelector('.page')
 
 const header = createHeader()
-
 page.append(header)
 
-const title = createElement('h1', 'title', 'Мій блог')
-
+const title = createElement('h1', 'title', 'Коммьюніті')
 page.append(title)
 
 // ====
 
-const POST_LIST = [
+// Створення кнопок з STICK_LIST
+const STICK_LIST = [
   {
-    category: [
-      { text: 'Важливо', id: 1 },
-      { text: 'Нова', id: 2 },
-    ],
-    info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
-    date: '24.01',
-    viewed: false,
+    text: 'База знань',
+    active: false,
   },
   {
-    category: [{ text: 'Нова', id: 2 }],
-    info: 'Ми хотіли, щоб у цьому чаті було близько 150 людей, щоб зробити якісний пак самопрезентацій.',
-    date: '24.01',
-    viewed: true,
+    text: 'Інформація',
+    active: true,
   },
 ]
 
 const createPost = () => {
-  const postList = createElement('main', 'post__list')
+  const stickList = createElement('div', 'stick__list')
 
-  POST_LIST.forEach((postData) => {
-    const post = createElement(
-      'div',
-      postData.viewed
-        ? 'post button post__viewed'
-        : 'post button',
+  STICK_LIST.forEach((postData) => {
+    const stick = createElement(
+      'button',
+      postData.active ? 'stick stick__active' : 'stick',
+      postData.text,
     )
-    const postHeader = createElement('div', 'post__header')
+    const divider = createElement('div', 'divider')
+    stick.append(divider)
 
-    const categoryList = createElement(
-      'div',
-      'post__category-list',
-    )
-    postData.category.forEach((category) => {
-      const categorySpan = createElement(
-        'span',
-        `post__category post__category--${category.id}`,
-        category.text,
-      )
-      categoryList.append(categorySpan)
-    })
+    stickList.append(stick)
 
-    const dateSpan = createElement(
-      'span',
-      'post__date',
-      postData.date,
-    )
-
-    postHeader.append(categoryList, dateSpan)
-
-    const infoParagrapf = createElement(
-      'p',
-      'post__info',
-      postData.info,
-    )
-    post.append(postHeader, infoParagrapf)
-
-    postList.append(post)
+    //  const stickList = createElement('div', 'stick__list')
   })
-  return postList
+  return stickList
 }
-
 const post = createPost()
 page.append(post)
+
+const createDesc = () => {
+  const desc = createElement('main', 'desc')
+
+  const img = createElement('img')
+  img.setAttribute('src', '/img/telegram.png')
+  img.setAttribute('width', '350')
+
+  desc.append(img)
+
+  const title = createElement(
+    'h2',
+    'title',
+    'Що таке база знань?',
+  )
+  desc.append(title)
+
+  const text = createElement(
+    'p',
+    'desc__text',
+    `База знань - це база даних, що містить правила виведення та інформацію про людське
+			досвід і знання в деякій
+			предметної галузі. У системах, що самонавчаються, база знань також містить інформацію, що є результатом
+			розв'язання попередніх завдань.`,
+  )
+
+  desc.append(text)
+
+  const buttonLink = createElement(
+    'button',
+    'button button__tg-link',
+    `Перейти до ком'юніті у Телеграм`,
+  )
+
+  desc.append(buttonLink)
+
+  return desc
+}
+// Додаємо список постів до сторінки
+const desc = createDesc()
+page.append(desc)
